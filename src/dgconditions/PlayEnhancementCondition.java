51fb74bc4bf7384ea3f6386305c5579b7021d2d2
CornyElephant <eldraco@mac.com>
2016-02-22 13:15:30 -0500

    Added comments describing checkCondition override method

--- src/dgconditions/PlayEnhancementCondition.java
+++ src/dgconditions/PlayEnhancementCondition.java
@@ -20,31 +20,42 @@ public class PlayEnhancementCondition extends Condition {
 		this.player = player;
 		this.enhanceables = new LinkedList<CreatureCard>();
 	}
-	
+	/**
+	 * Gets a list of all enhanceable creature cards this condition found
+	 * @return All creature cards that are legal for enhancement
+	 */
 	public List<CreatureCard> getEnhanceables()
 	{
 		return enhanceables;
 	}
 
+	/**
+	 * Checks if there are any creatures on the battlefield that can be enhanced
+	 * @return true if there are any currently enhanceable creatures, false if there are none
+	 */
 	@Override
 	protected boolean checkCondition() {
 		
 		List<CreatureCard> creaturesToCheck = new LinkedList<CreatureCard>();
 		List<DGPlayer> dgPlayers = new LinkedList<DGPlayer>();
+		//add all players to a linkedlist (does this need to be done? Why not just use list from PlayerOrder)
 		for(Player player : Engine.getCurrentGame().getPlayerOrder().getPlayers())
 		{
 			dgPlayers.add((DGPlayer) player);
 		}
+		//add all creature cards from each field to a list of creatures to check
 		for(DGPlayer dgPlayer : dgPlayers)
 		{
 			creaturesToCheck.addAll(dgPlayer.getField().getCreatureCards());
 		}
+		//add all creatures that are enhanceable and can currently be enhanced
 		List<CreatureCard> validCreatures = new LinkedList<CreatureCard>();
 		for(CreatureCard creature : creaturesToCheck)
 		{
 			if(creature instanceof Enhanceable && creature.canBeEnhanced())
 				validCreatures.add(creature);
 		}
+		//clear current enhanceables and add the current list
 		enhanceables.clear();
 		enhanceables.addAll(validCreatures);
 		if(enhanceables.isEmpty())
